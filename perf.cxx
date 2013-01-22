bbd25ea966caa225e402c25b18e9681d38e42b15
Curtis Rueden <ctrueden@wisc.edu>
2013-01-22 12:08:14 -0600

    Eliminate some more whitespace errors

--- perf.cxx
+++ perf.cxx
@@ -31,7 +31,7 @@ int main(int, char * argv[])
   const unsigned int dim = 3;
   typedef unsigned char PType;
   typedef itk::Image< PType, dim >    IType;
-  
+
   // read the input image
   typedef itk::ImageFileReader< IType > ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
@@ -40,7 +40,7 @@ int main(int, char * argv[])
   reader->SetImageIO( io );
   // update a first time to avoid the effect of the system cache
   reader->Update();
-  
+
 //   save the image so it can be used by other readers to compare
 //   typedef itk::ImageFileWriter< IType > WriterType;
 //   WriterType::Pointer writer = WriterType::New();
@@ -56,21 +56,21 @@ int main(int, char * argv[])
   reader2->SetImageIO( io2 );
   // update a first time to avoid the effect of the system cache
   reader2->Update();
-  
+
 //   ReaderType::Pointer reader3 = ReaderType::New();
 //   reader3->SetFileName( "out.tif" );
 //   itk::TIFFImageIO::Pointer io3 = itk::TIFFImageIO::New();
 //   reader3->SetImageIO( io3 );
 //   // update a first time to avoid the effect of the system cache
 //   reader3->Update();
-//   
+//
 //   ReaderType::Pointer reader4 = ReaderType::New();
 //   reader4->SetFileName( "out.nrrd" );
 //   itk::NrrdImageIO::Pointer io4 = itk::NrrdImageIO::New();
 //   reader4->SetImageIO( io4 );
 //   // update a first time to avoid the effect of the system cache
 //   reader4->Update();
-  
+
   std::cout << "1IO\txIO\txTIF\txNRRD" << std::endl;
 
   itk::TimeProbe time;
@@ -83,7 +83,7 @@ int main(int, char * argv[])
     time.Start();
     reader->Update();
     time.Stop();
-    
+
     io2 = itk::SCIFIOImageIO::New();
     reader2->SetImageIO( io2 );
     time2.Start();
@@ -95,19 +95,19 @@ int main(int, char * argv[])
 //     time3.Start();
 //     reader3->Update();
 //     time3.Stop();
-//     
+//
 //     io4 = itk::NrrdImageIO::New();
 //     reader4->SetImageIO( io4 );
 //     time4.Start();
 //     reader4->Update();
 //     time4.Stop();
-    
+
   std::cout << std::setprecision(3)
-    << time.GetMeanTime() << "\t" 
-    << time2.GetMeanTime() << "\t" 
-//     << time3.GetMeanTime() << "\t" 
-//     << time4.GetMeanTime() << "\t" 
-    << std::endl;  
+    << time.GetMeanTime() << "\t"
+    << time2.GetMeanTime() << "\t"
+//     << time3.GetMeanTime() << "\t"
+//     << time4.GetMeanTime() << "\t"
+    << std::endl;
     }
 
   return 0;
