2a4c7c50216144265b8fc0510125f3dc7fe17acd
Curtis Rueden <ctrueden@wisc.edu>
2013-01-24 13:43:54 -0600

    Fix itkSCIFIOImageInfoTest argument handling
    This commit works around the fact that the CMakeLists.txt (as written)
    passes all arguments as a single quoted argument. There is almost
    certainly a better way to do it in CMake, but I don't know how, so we
    split the argument in C++ land instead.
    
    This change also assigns the SCIFIO ImageIO to the reader properly.

--- test/itkSCIFIOImageInfoTest.cxx
+++ test/itkSCIFIOImageInfoTest.cxx
@@ -15,54 +15,101 @@
  *  limitations under the License.
  *
  *=========================================================================*/
+#include <fstream>
 #include <iostream>
-#include <vector>
+#include <sstream>
 #include <string>
+#include <vector>
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 #include "itkImage.h"
 #include "itkMetaDataObject.h"
 #include "itkMetaDataDictionary.h"
 #include "itkImageIOBase.h"
-//#include "itkSCIFIOImageIO.h"
+#include "itkSCIFIOImageIO.h"
 
 #define METADATA_NOT_FOUND "No value for this key."
 
+namespace
+{
+  /*
+   * Splits a string into tokens using the given delimiter.
+   *
+   * Thanks to SO #236129 for this solution:
+   * http://stackoverflow.com/a/236803
+   */
+  std::vector<std::string> &split( const std::string &s, char delim,
+                                   std::vector<std::string> &elems )
+  {
+    std::stringstream ss(s);
+    std::string item;
+    while(std::getline(ss, item, delim)) {
+      elems.push_back(item);
+    }
+    return elems;
+  }
+
+  std::vector<std::string> split( const std::string &s, char delim )
+  {
+    std::vector<std::string> elems;
+    return split(s, delim, elems);
+  }
+}
+
 int itkSCIFIOImageInfoTest( int argc, char * argv[] )
 {
-  std::cout << "--------- asdfasdfasdf ----------" << std::endl;
-  if( argc < 4)
+  if( argc < 2)
   {
-    std::cerr << "Usage: " << argv[0] << " sizeZ sizeC sizeT\n";
+    std::cerr << "Usage: " << argv[0] << " argument\n";
     return EXIT_FAILURE;
   }
 
-  char* sZ = argv[1];
-  char* sC = argv[2];
-  char* sT = argv[3];
-  std::string inputFile = "scifioImageInfo"
-                          "&sizeZ=" + std::string(sZ) +
-                          "&sizeC=" + std::string(sC) +
-                          "&sizeT=" + std::string(sT) +
-                          ".fake";
-  int sizeZ = atoi(sZ);
-  int sizeC = atoi(sC);
-  int sizeT = atoi(sT);
+  // NB: We pass all arguments as a single argument, to work around our
+  // lack of understanding of CMake. I could not immediately wrangle CMake
+  // into passing the arguments without them being quoted as a single one,
+  // so instead we simply split the arguments here, which suffices.
+  std::vector<std::string> argList = split(std::string(argv[1]), ' ');
+
+  if( argList.size() != 3 )
+  {
+    std::cerr << "Argument not of the form: sizeZ sizeC sizeT\n";
+    return EXIT_FAILURE;
+  }
+  std::string sZ = argList[0];
+  std::string sC = argList[1];
+  std::string sT = argList[2];
+
+  // Create a fake file on disk, for use with testing the SCIFIO ImageIO
+  // reader. SCIFIO does not actually care whether the file exists.
+  std::string id = "scifioImageInfo"
+                   "&sizeZ=" + sZ +
+                   "&sizeC=" + sC +
+                   "&sizeT=" + sT +
+                   ".fake";
+
+  int sizeZ = atoi(sZ.c_str());
+  int sizeC = atoi(sC.c_str());
+  int sizeT = atoi(sT.c_str());
+
   std::cout << "--------- itkSCIFIOImageInfoTest ----------" << std::endl;
 
   typedef unsigned char     PixelType;
   const unsigned int        Dimension = 3;
 
   typedef itk::Image< PixelType, Dimension >  ImageType;
-
   typedef itk::ImageFileReader<ImageType> ReaderType;
 
+  itk::SCIFIOImageIO::Pointer io = itk::SCIFIOImageIO::New();
+
+  io->DebugOn();
+
   ReaderType::Pointer reader = ReaderType::New();
-  ImageType::Pointer img;
+  reader->SetImageIO(io);
 
   reader->SetFileName(inputFile);
   reader->Update();
-  img = reader->GetOutput();
+
+  ImageType::Pointer img = reader->GetOutput();
 
   std::string metaString (METADATA_NOT_FOUND);
   itk::MetaDataDictionary imgMetaDictionary = img->GetMetaDataDictionary();
