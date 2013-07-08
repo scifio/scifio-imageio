e69e9580dadc7b2fe638a40a5aab57fa9b5ee0fb
Mark Hiner <hinerm@gmail.com>
2013-07-08 15:19:56 -0500

    Refactored ImageIOTest parameters
    Changed itkSCIFIOImageIOTest to behave more like a *nix command line
    application, with parameters passable through - abbreviated or
    --explicit arguments.
    
    The following can now be set:
    * series range (for multi-series datasets)
    * number of dimensions
    * pixel type
    * number of streaming divisions
    
    This system should be easily extensible without concerns of parameter
    order.
    
    In the future, the RGB and Vector tests should be removed in favor of
    additional flags for this test.

--- test/itkSCIFIOImageIOTest.cxx
+++ test/itkSCIFIOImageIOTest.cxx
@@ -23,25 +23,123 @@
 #include "itkMetaDataObject.h"
 #include "itkStreamingImageFilter.h"
 
+int fail(char * argv [])
+{
+  std::cerr << "Usage: " << argv[0] << " input output [OPTIONS]\n\nOPTIONS:\n-v <n>, --divs <n>\n\tUse n streaming divisions\n-s <n1 n2>, --series <n1 n2>\n\tAfter reading the first series specified by @@ notation (0 default) will read all series from n1+1 to n2, exclusive\n-d <2-5>, --dims <2-5>\n\tSets the dimensionality. This should be equal to or less than your target image\'s dimensionality. If less, dimensions will be truncated in reverse dimension order\n-t <T>, --type <T>\n\tSets the pixel type. T should be one of: int, uint, short, ushort, float, long, double. Default is ushort.\n";
+  return EXIT_FAILURE;
+}
+
 int itkSCIFIOImageIOTest( int argc, char * argv [] )
 {
   if( argc < 3)
     {
-    std::cerr << "Usage: " << argv[0] << " input output [numberOfStreamDivisions]\n";
-    return EXIT_FAILURE;
+    return fail(argv);
     }
   const char * inputFileName = argv[1];
   const char * outputFileName = argv[2];
+
   std::string numberOfStreamDivisions = "4";
-  if( argc > 3 )
+  int seriesStart = 0;
+  int seriesEnd = 1;
+  typedef unsigned short       PixelType;
+  typedef itk::Image< PixelType, 2 >   ImageType;
+
+  for (int i = 3; i < argc; i++)
     {
-    numberOfStreamDivisions = argv[3];
+    if (strcmp (argv[i], "-v") == 0 || strcmp (argv[i], "--divs") == 0)
+      {
+      if (i + 1 >= argc)
+        {
+        return fail(argv);
+        }
+      numberOfStreamDivisions = argv[i+1];
+      i++;
+      }
+    else if (strcmp (argv[i], "-s") == 0 || strcmp (argv[i], "--series") == 0)
+      {
+      if (i + 2 >= argc)
+        {
+        return fail(argv);
+        }
+      seriesStart = atoi(argv[i+1]);
+      seriesEnd = atoi(argv[i+2]);
+      i+=2;
+      }
+    else if (strcmp (argv[i], "-t") == 0 || strcmp (argv[i], "--type") == 0)
+      {
+      if (i + 1 >= argc)
+        {
+        return fail(argv);
+        }
+      std::string ptype = argv[i+1];
+      i++;
+
+      if (ptype == "int")
+        {
+        typedef int                  PixelType;
+        }
+      else if (ptype == "uint")
+        {
+        typedef unsigned int         PixelType;
+        }
+      else if (ptype == "short")
+        {
+        typedef short                PixelType;
+        }
+      else if (ptype == "ushort")
+        {
+        typedef unsigned short       PixelType;
+        }
+      else if (ptype == "long")
+        {
+        typedef long                 PixelType;
+        }
+      else if (ptype == "float")
+        {
+        typedef float                PixelType;
+        }
+      else if (ptype == "double")
+        {
+        typedef double               PixelType;
+        }
+      else
+        {
+        return fail(argv);
+        }
+      }
     }
 
-  typedef unsigned short       PixelType;
-  const unsigned int           Dimension = 2;
+  // Have to check dimension size after PixelType, since the ImageType is being redefined.
+  for (int i = 3; i < argc; i++)
+    {
+    if (strcmp (argv[i], "-d") == 0 || strcmp (argv[i], "--dims") == 0)
+      {
+      if (i + 1 >= argc)
+        {
+        return fail(argv);
+        }
+      std::string dim = argv[i+1];
+      i++;
 
-  typedef itk::Image< PixelType, Dimension >   ImageType;
+      if (dim == "2")
+        {
+        typedef itk::Image< PixelType, 2 >   ImageType;
+        }
+      else if (dim == "3")
+        {
+        typedef itk::Image< PixelType, 3 >   ImageType;
+        }
+      else if (dim == "4")
+        {
+        typedef itk::Image< PixelType, 4 >   ImageType;
+        }
+      else if (dim == "5")
+        {
+        typedef itk::Image< PixelType, 5 >   ImageType;
+        }
+      }
+    }
+
   typedef itk::ImageFileReader< ImageType >    ReaderType;
 
   itk::SCIFIOImageIO::Pointer io = itk::SCIFIOImageIO::New();
@@ -50,32 +148,9 @@ int itkSCIFIOImageIOTest( int argc, char * argv [] )
 
   ReaderType::Pointer reader = ReaderType::New();
   std::cout << "reader->GetUseStreaming(): " << reader->GetUseStreaming() << std::endl;
-
-  std::cout << "done checking streaming usage";
-
+  std::cout << "done checking streaming usage" << std::endl;
 
   reader->SetImageIO(io);
-  /*
-  reader->SetFileName("fileNotFound");
-
-  // should get an exception
-  // TODO: Exception is thrown in Java not c++ .. is not caught and dies
-  bool catched = false;
-  try
-    {
-    reader->Update();
-    }
-  catch (itk::ExceptionObject &e)
-    {
-    std::cerr << e << std::endl;
-    catched = true;
-    }
-  if( !catched )
-    {
-    std::cerr << "exception not catched for wrong path" << std::endl;
-    return EXIT_FAILURE;
-    }
-*/
 
   reader->SetFileName( inputFileName );
 
@@ -91,16 +166,32 @@ int itkSCIFIOImageIOTest( int argc, char * argv [] )
   //
   writer = WriterType::New();
   writer->SetInput( streamer->GetOutput() );
-  writer->SetFileName( outputFileName );
 
-  try
+  while ( seriesStart < seriesEnd )
     {
-    writer->Update();
-    }
-  catch (itk::ExceptionObject &e)
-    {
-    std::cerr << e << std::endl;
-    return EXIT_FAILURE;
+
+    std::stringstream ss;
+    ss << seriesStart;
+
+    std::string fileOut = ss.str() + outputFileName;
+
+    writer->SetFileName( fileOut );
+
+    try
+      {
+      writer->Update();
+      }
+    catch (itk::ExceptionObject &e)
+      {
+      std::cerr << e << std::endl;
+      return EXIT_FAILURE;
+      }
+    seriesStart++;
+
+    if (seriesStart < seriesEnd)
+      {
+      io->SetSeries(seriesStart);
+      }
     }
 
   std::string notes;
