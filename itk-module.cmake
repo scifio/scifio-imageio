b42d5ce6fe21afc61dc5207c5669105ef28c2df7
Mark Hiner <hinerm@gmail.com>
2013-08-26 11:32:39 -0500

    Removed "ITK" from module name
    In response to the latest PnP for remote modules, changed the module
    name from "ITKIOSCIFIO" to just "SCIFIO".
    
    See
    http://www.itk.org/Wiki/ITK/Policy_and_Procedures_for_Adding_Remote_Modules

--- itk-module.cmake
+++ itk-module.cmake
@@ -2,7 +2,7 @@ set(DOCUMENTATION "This module contains classes for reading and writing image
 files with the Open Microscopy Envionment (OME) Scientific Imaging Formats
 Input and Output (SCIFIO) library.")
 
-itk_module(ITKIOSCIFIO
+itk_module(SCIFIO
   DEPENDS
     ITKIOImageBase
   TEST_DEPENDS
